c3e1a02a5bfdf36d5e6f9f899af450d85e029ecc
Vitaliy Buchinchik <49568601+vitaliy-buchinchik@users.noreply.github.com>
2022-07-22 16:37:23 +0300

    Add files via upload
    Prelend fixes

--- preland.js
+++ preland.js
@@ -1,12 +1,3 @@
-const uaBtn = document.getElementById("button_ua");
-const enBtn = document.getElementById("button_en");
-const esBtn = document.getElementById("button_es");
-const itBtn = document.getElementById("button_it");
-const deBtn = document.getElementById("button_de");
-const csBtn = document.getElementById("button_cs");
-const plBtn = document.getElementById("button_pl");
-const frBtn = document.getElementById("button_fr");
-
 const uaGarbageLink = document.getElementById("garbage_removal_link_ua");
 const enGarbageLink = document.getElementById("garbage_removal_link_en");
 const esGarbageLink = document.getElementById("garbage_removal_link_es");
@@ -16,25 +7,41 @@ const csGarbageLink = document.getElementById("garbage_removal_link_cs");
 const plGarbageLink = document.getElementById("garbage_removal_link_pl");
 const frGarbageLink = document.getElementById("garbage_removal_link_fr");
 
-const garbage_links = [uaGarbageLink, enGarbageLink, esGarbageLink, itGarbageLink, deGarbageLink, csGarbageLink, plGarbageLink, frGarbageLink];
+const uaProductLink = document.getElementById("products_link_ua");
+const enProductLink = document.getElementById("products_link_en");
+const esProductLink = document.getElementById("products_link_es");
+const itProductLink = document.getElementById("products_link_it");
+const deProductLink = document.getElementById("products_link_de");
+const csProductLink = document.getElementById("products_link_cs");
+const plProductLink = document.getElementById("products_link_pl");
+const frProductLink = document.getElementById("products_link_fr");
 
-console.log(uaGarbageLink.style);
+const garbageLinks = [uaGarbageLink, enGarbageLink, esGarbageLink, itGarbageLink, deGarbageLink, csGarbageLink, plGarbageLink, frGarbageLink];
+const productLinks = [uaProductLink, enProductLink, esProductLink, itProductLink, deProductLink, csProductLink, plProductLink, frProductLink];
 
-function prelandLanguages(link) {
-  for (let i = 0; i < garbage_links.length; i++) {
-    if (garbage_links[i] == link) {
-      garbage_links[i].style.display = "flex";
+function prelandLanguages(garbageLink, productLink) {
+  for (let i = 0; i < garbageLinks.length; i++) {
+    if (garbageLinks[i] == garbageLink) {
+      garbageLinks[i].style.display = "flex";
     } else {
-      garbage_links[i].style.display = "none";
+      garbageLinks[i].style.display = "none";
+    }
+  }
+
+  for (let i = 0; i < productLinks.length; i++) {
+    if (productLinks[i] == productLink) {
+      productLinks[i].style.display = "flex";
+    } else {
+      productLinks[i].style.display = "none";
     }
   }
 }
 
-uaBtn.addEventListener("click", () => {prelandLanguages(uaGarbageLink)});
-enBtn.addEventListener("click", () => {prelandLanguages(enGarbageLink)});
-esBtn.addEventListener("click", () => {prelandLanguages(esGarbageLink)});
-itBtn.addEventListener("click", () => {prelandLanguages(itGarbageLink)});
-deBtn.addEventListener("click", () => {prelandLanguages(deGarbageLink)});
-csBtn.addEventListener("click", () => {prelandLanguages(csGarbageLink)});
-plBtn.addEventListener("click", () => {prelandLanguages(plGarbageLink)});
-frBtn.addEventListener("click", () => {prelandLanguages(frGarbageLink)});
+const uaBtn = document.getElementById("button_ua").addEventListener("click", () => {prelandLanguages(uaGarbageLink, uaProductLink)});
+const enBtn = document.getElementById("button_en").addEventListener("click", () => {prelandLanguages(enGarbageLink, enProductLink)});
+const esBtn = document.getElementById("button_es").addEventListener("click", () => {prelandLanguages(esGarbageLink, esProductLink)});
+const itBtn = document.getElementById("button_it").addEventListener("click", () => {prelandLanguages(itGarbageLink, itProductLink)});
+const deBtn = document.getElementById("button_de").addEventListener("click", () => {prelandLanguages(deGarbageLink, deProductLink)});
+const csBtn = document.getElementById("button_cs").addEventListener("click", () => {prelandLanguages(csGarbageLink, csProductLink)});
+const plBtn = document.getElementById("button_pl").addEventListener("click", () => {prelandLanguages(plGarbageLink, plProductLink)});
+const frBtn = document.getElementById("button_fr").addEventListener("click", () => {prelandLanguages(frGarbageLink, frProductLink)});
